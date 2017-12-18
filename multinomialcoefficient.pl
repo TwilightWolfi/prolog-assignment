735162bd7d5bd8b769cb56c4da6b259d85d32332
HugeWollman <34238375+HugeWollman@users.noreply.github.com>
2017-12-18 00:03:40 +0000

    Add files via upload

--- multinomialcoefficient.pl
+++ multinomialcoefficient.pl
@@ -1,14 +1,14 @@
 
-listSum(L,X) :- listSum(L,V,X).
+listSum(L,X) :- listSum(L,0,X).
 listSum([],X,X).
 listSum([H|T],V,X) :- Y is V + H, listSum(T,Y,X).
 
-factorial(X,Y) :- factorial(X,V,Y).
+factorial(X,Y) :- factorial(X,1,Y).
 factorial(0,Y,Y).
 factorial(X,V,Y) :- W is V*X, Z is X-1, factorial(Z,W,Y).
 
-listFactorials(L,X) :- listFactorials(L,V,X).
+listFactorials(L,X) :- listFactorials(L,1,X).
 listFactorials([],X,X).
-listFactorials([H|T],V,X) :- factorial(H,Y), W is V+Y, listFactorials(T,W,X).
+listFactorials([H|T],V,X) :- factorial(H,Y), W is V*Y, listFactorials(T,W,X).
 
 multinomial(L,X) :- listSum(L,Y), factorial(Y,Z), listFactorials(L,V), X is Z/V.
